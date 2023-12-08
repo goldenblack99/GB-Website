7446ad5ad549fb631d35b178d9ca6e314c847e81
goldenblack99 <148334712+goldenblack99@users.noreply.github.com>
2023-12-08 19:41:06 +0530

    Add files via upload

---
+++ Beta.js
@@ -0,0 +1,6 @@
+function hamburger() {
+	var hamburgerbtn = document.getElementById('hamburger');
+	hamburgerbtn.classList.toggle('hamburger-on');
+	var hamburgerbtn = document.getElementById('navigation');
+	hamburgerbtn.classList.toggle('navigation-open');
+}
